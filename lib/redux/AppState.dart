4ac5ed04e262ba8bf9769ec3545e0646ec41f4df
xt <xutao128@sina.cn>
2019-12-05 13:50:47 +0800

    reduce添加注释

--- lib/redux/AppState.dart
+++ lib/redux/AppState.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:redux/redux.dart';
 
-class AppState { ///可以定义多个State,这里之定义了一个
+class AppState { ///可以定义多个State状态属性,这里只定义了一个
     Color themeColor;
     AppState({this.themeColor});
 }
@@ -14,7 +14,9 @@ AppState appReducer(AppState state, dynamic action) { ///每一个State都由一
 ///每个Reducer只能返回一个状态，使用flutter_redux中的combineReducers来整合多个Reducer
 ///通过combineReducers和TypedReducer将我们的Action和_refresh方法进行绑定，_refresh方法返回一个新的State
 final ThemeDataReducer = combineReducers<Color>([
-    TypedReducer<Color, RefreshThemeDataAction>(_refresh), //将RefreshThemeDataAction与_refresh方法绑定
+    ///将RefreshThemeDataAction与_refresh方法绑定，即action是RefreshThemeDataAction 使用_refresh来处理
+    TypedReducer<Color, RefreshThemeDataAction>(_refresh),
+    ///可以添加其他的Action和相关方法
 ]);
 
 Color _refresh(Color themeColor, action) {
